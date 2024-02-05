6f924c45a87d76c4777f1c7cc3f10d1aeb9b20fd
RicTieri <tieri.riccardo@gmail.com>
2024-02-05 17:37:02 +0100

    added model, controller, migration and seeder with array issue

---
+++ database/seeders/ComicsSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comic;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $comics = config('db');
+        foreach ($comics as $comic) {
+            Comic::create([
+                'title' => $comic['title'],
+                'description' => $comic['description'],
+                'thumb' => $comic['thumb'],
+                'price' => $comic['price'],
+                'series' => $comic['series'],
+                'sale_date' => $comic['sale_date'],
+                'type' => $comic['type'],
+                'artists' => $comic['artists'],
+                'writers' => $comic['writers'],
+            ]);
+        }
+    }
+}
